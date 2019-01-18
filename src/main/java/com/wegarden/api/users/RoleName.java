fa085a5bc7ee3b4d5f633994fbb9fcbef0097d9a
Patrick <patson312@gmail.com>
2019-01-18 16:58:32 +0100

    add user roles

---
+++ src/main/java/com/wegarden/api/users/RoleName.java
@@ -0,0 +1,6 @@
+package com.wegarden.api.users;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
--- src/main/java/com/wegarden/api/users/User.java
+++ src/main/java/com/wegarden/api/users/User.java
@@ -3,13 +3,12 @@ package com.wegarden.api.users;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.annotations.NaturalId;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class User {
@@ -30,8 +29,16 @@ public class User {
 
     @NotBlank
     @Size(max = 100)
-    @JsonIgnore
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
+
 
     public User(String username, String email, String password) {
         this.username = username;
